04e7136b295ad52ffdb3424f264b29113605d9ed
fyarenozcelik <yrn.ozclk71@gmail.com>
2024-10-31 15:12:29 +0300

    quiz 5 tamamlandı

---
+++ src/main/resources/db/migration/V1_000__createTables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE test (
+                      id SERIAL PRIMARY KEY,
+                      name VARCHAR(50) NOT NULL,
+                      surname VARCHAR(50) NOT NULL,
+                      password VARCHAR(100) NOT NULL
+);
