d63d540e4e8fa30d7978c0147bb89c7b6def0b19
fyarenozcelik <yrn.ozclk71@gmail.com>
2024-10-31 14:24:44 +0300

    lk Spring Boot projesi eklendi

---
+++ src/main/java/com/example/q5_152120201028/Q5152120201028Application.java
@@ -0,0 +1,13 @@
+package com.example.q5_152120201028;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q5152120201028Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q5152120201028Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/q5_152120201028/Q5152120201028ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.q5_152120201028;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q5152120201028ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
